a4ee8409a20ec5654c929159570d5265ca1ed024
Maiara <maiaraborgesvieira@gmail.com>
2022-06-01 11:24:45 -0300

    alterações main
    foram incluidas conversões variadas de tipos primitivos

--- conceitos_intent/app/src/main/java/formacao/desenvolvedores/tecnologia/uno/porjetodesoftwareorientadoaobjetos/conceitosintent/MainActivity.java
+++ conceitos_intent/app/src/main/java/formacao/desenvolvedores/tecnologia/uno/porjetodesoftwareorientadoaobjetos/conceitosintent/MainActivity.java
@@ -1,15 +1,12 @@
 package formacao.desenvolvedores.tecnologia.uno.porjetodesoftwareorientadoaobjetos.conceitosintent;
 
-import androidx.activity.result.ActivityResult;
-import androidx.activity.result.ActivityResultCallback;
-import androidx.activity.result.ActivityResultLauncher;
-import androidx.activity.result.contract.ActivityResultContracts;
+
+
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.Activity;
 import android.content.Intent;
-import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -19,14 +16,16 @@ import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import formacao.desenvolvedores.tecnologia.uno.porjetodesoftwareorientadoaobjetos.conceitosintent.utils.app.UtilsApp;
+
 public class MainActivity extends AppCompatActivity {
+    public static final String TAG = "MainActivity";
     private Button btnPerguntar;
-    private TextView tvExibirResposta;
+    private TextView tvExibirRespota;
     private TextView tvTitulo;
     private EditText edtPergunta;
     private ImageButton btnDelete;
     public static final int REQUEST_CODE = 5;
-    private ActivityResultLauncher<Intent> activityResultLauncher;
 
 
     @Override
@@ -34,97 +33,77 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ActionBar actionBar = getSupportActionBar();
+        if(actionBar != null){
+            actionBar.setTitle("Activity de Perguntas");
+        }
 
-        btnPerguntar     = findViewById(R.id.btnPerguntar);
-        tvExibirResposta = findViewById(R.id.tvExibirResposta);
-        edtPergunta      = findViewById(R.id.edtPergunta);
-        btnDelete        = findViewById(R.id.btnDelete);
-        tvTitulo         = findViewById(R.id.tvTitulo);
 
+        btnPerguntar    = findViewById(R.id.btnPerguntar);
+        tvExibirRespota = findViewById(R.id.tvExibirResposta);
+        edtPergunta     = findViewById(R.id.edtPergunta);
+        btnDelete       = findViewById(R.id.btnDelete);
+        tvTitulo        = findViewById(R.id.tvTitulo);
         tvTitulo.setVisibility(View.INVISIBLE);
-
-        Bundle extras = getIntent().getExtras();
-        String pergunta = "";
-        if(!edtPergunta.getText().toString().isEmpty()) {
-
-        btnDelete.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                edtPergunta.setText("");
-                tvExibirResposta.setText("");
-            }
-        });
-    }
 
 
         btnPerguntar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (!edtPergunta.getText().toString().isEmpty()) {
+                if(!edtPergunta.getText().toString().isEmpty()){
+                    Intent irParaOutraActivity = new Intent(MainActivity.this, RespostaActivity.class);
 
-                    if (Build.VERSION.SDK_INT < Build.VERSION_CODES.R) {
-                        Intent irParaOutraActivity = new Intent(MainActivity.this, RespostaActivity.class);
+                    String conteudo = edtPergunta.getText().toString();
+                    irParaOutraActivity.putExtra("Pergunta", conteudo);
 
-                        String conteudo = edtPergunta.getText().toString();
-                        irParaOutraActivity.putExtra("Pergunta", conteudo);
+                    startActivityForResult(irParaOutraActivity, REQUEST_CODE);
 
-                        if (!tvExibirResposta.getText().toString().isEmpty()) {
-                            String myResposta = tvExibirResposta.getText().toString();
-                            irParaOutraActivity.putExtra("Resposta", myResposta);
-                        }
-                        startActivityForResult(irParaOutraActivity, REQUEST_CODE);
 
-                    } else {
-                        openActivityForResult();
-                    }
-                }else {
+                } else {
                     Toast.makeText(MainActivity.this, "Insira uma pergunta", Toast.LENGTH_LONG).show();
                 }
             }
         });
 
-        activityResultLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
-
+        btnDelete.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onActivityResult(ActivityResult result) {
-                if (result.getResultCode() == Activity.RESULT_OK){
-                    Intent data = result.getData();
-                    //tvExibirResposta.setText(data.getExtras().toString());
-                    tvTitulo.setVisibility(View.VISIBLE);
-                    tvExibirResposta.setText(data.getExtras().getString("returnData"));
-                }
+            public void onClick(View v) {
+                edtPergunta.setText("");
+                tvExibirRespota.setText("");
             }
         });
+
+    UtilsApp utilsApp = new UtilsApp();
+    Log.d(TAG, "Valor convertido de tipos primitivos float p/ int: " + utilsApp.convertToInt(5.1987));
+
+    byte b = -27;
+    Log.d(TAG, "Valor convertido de tipos primitivos byte p/ int: " + utilsApp.convertToInt(b));
+
+    long valorLong = 9223372036854775800L;
+    Log.d(TAG, "Valor convertido de tipos primitivos long p/ int: " + utilsApp.convertToInt(valorLong));
+
+
+
+
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if ((requestCode == REQUEST_CODE) && (resultCode == RESULT_OK)) {
+        if((requestCode == REQUEST_CODE) && (resultCode == RESULT_OK)){
 
             String returnString = data.getExtras().getString("returnData");
 
-            if (returnString != null) {
-                 if (!returnString.isEmpty()) {
-                       tvTitulo.setVisibility(View.VISIBLE);
-                            //edtPergunta.setText(returnString);
+            if(returnString != null){
+                if(!returnString.isEmpty()){
+                    tvTitulo.setVisibility(View.VISIBLE);
+                    //edtPergunta.setText(returnString);
+
                 }
             }
-            tvExibirResposta.setText(returnString);
+
+            tvExibirRespota.setText(returnString);
         }
     }
-
-    private void openActivityForResult(){
-        Intent intent = new Intent(MainActivity.this, RespostaActivity.class);
-
-        /*String myString = edtPergunta.getText().toString();
-        intent.putExtra("Exibir Pergunta", myString);*/
-
-        intent.putExtra("Exibir Pergunta", edtPergunta.getText().toString());
-        activityResultLauncher.launch(intent);
-
-
-    }
-
 }
